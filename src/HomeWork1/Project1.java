fb8c68219cac649f7c33fa325223ada7390e6065
Shovona <shovona@gmail.com>
2021-07-08 21:43:54 -0400

    my homework 1 and 2

---
+++ src/HomeWork1/Project1.java
@@ -0,0 +1,60 @@
+package HomeWork1;
+
+public class Project1 {
+
+    // main method
+    public static void main(String[] args) {
+
+    byte numberOfHoursInaDay = 24;
+
+        System.out.println("\nnumberOfHoursInaDay:24 ");
+
+    short maxNumberOfDaysInYear = 366;
+
+        System.out.println("\nmaxNumberOfDaysInYear:366");
+
+    int totalNumberOfEmployeesInIbm = 50000;
+
+        System.out.println("\ntotalNumberOfEmployeesInIbm:50000");
+
+    long numberOfPopulation = 2000000000L;
+
+        System.out.println("\nnumberOfPopulation : 2000000000L");
+
+    float interestRate = 1.9f;
+        System.out.println("\nCurrent interest rate:" + interestRate);
+
+    long accountBalance = 50000000L;
+        System.out.println("\naccountBalance = 50000000L");
+
+    char char1 = 's';
+
+        System.out.println("myInitial:" + char1);
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
